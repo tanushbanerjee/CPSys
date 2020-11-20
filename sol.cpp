29deea14387b38c8b9c399da19f79ad3c61961ed
Tanush Banerjee <samihanbanerjee026@gmail.com>
2020-11-20 17:30:44 +0530

    INITIAL COMMIT

---
+++ sol.cpp
@@ -0,0 +1,26 @@
+/*******************************************************************************
+Author:  Tanush Banerjee 
+Email:  samihanbanerjee026@gmail.com
+CodeForces Username: samihanbanerjee026
+CodeChef Username: tanushbanerjee
+ *******************************Macros And Includes******************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define deb(x) cout << #x << " " << x << endl;
+#define fo(i, n) for(i=0; i < n; ++i)
+#define Fo(i, k, n) for(i=k; i<n; ++i)
+#define rv(i, n, a) for(int i = 0; i < n; ++i) {cin >> a[i];}
+#define max(i, j) (i < j) ? cout<<1:cout<<0;
+#define pb(x) push_back(x);
+using ll = long long;
+using l = long;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ttl int tt;cin>>tt;while(tt--)
+ 
+
+int main()
+{
+	fastio;
+	return 0;
+}
